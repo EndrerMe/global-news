44519d796a4cb11c443f52807780388428c5ec4d
Alexei Fomin <1stan@DESKTOP-U84NEP9>
2019-10-30 17:44:01 +0200

    changes

--- src/shared/layouts/header/header.vue
+++ src/shared/layouts/header/header.vue
@@ -396,7 +396,7 @@ export default {
   align-items: center;
 }
 .top-menu-wrap .wide-menu li a svg {
-  margin: 0 20px;
+  margin: 0 15px;
   color: rgb(248, 198, 26);
 }
 .top-menu-wrap .wide-menu li.weather-wrap {
