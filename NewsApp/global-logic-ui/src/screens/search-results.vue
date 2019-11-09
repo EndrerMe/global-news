1e50427b83a5b9d46045e6c69843de043739fcd4
Alexei Fomin <1stan@DESKTOP-U84NEP9>
2019-11-09 11:14:49 +0200

    search input

--- NewsApp/global-logic-ui/src/screens/search-results.vue
+++ NewsApp/global-logic-ui/src/screens/search-results.vue
@@ -5,6 +5,10 @@
         <div class="search-panel-content">
           <div class="search-input-wrap">
             <input id="search-results-input" placeholder="search" />
+            <label class="close-search-wrap">
+              <span class="close-search"></span>
+            </label>
+
             <label for="search-results-input" class="search-icon-wrap">
               <font-awesome-icon icon="search" class="fa-lg" />
             </label>
@@ -23,14 +27,80 @@
   background: #052962;
 }
 
+.search-panel-wrap .search-panel-content .search-input-wrap {
+  position: relative;
+}
 .search-panel-wrap .search-panel-content .search-input-wrap input {
   width: 100%;
   height: 50px;
+  font-family: "Poppins-Regular";
+  font-size: 16px;
+  padding-left: 15px;
 }
-.search-panel-wrap .search-panel-content .search-input-wrap label.search-icon-wrap{
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.search-icon-wrap {
   position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+  right: -40px;
 }
-.search-panel-wrap .search-panel-content .search-input-wrap label.search-icon-wrap svg{
-    color: white;
+
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.close-search-wrap {
+  position: absolute;
+  right: 20px;
+  top: 5px;
+}
+
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.close-search-wrap
+  span {
+  display: block;
+  position: relative;
+  width: 40px;
+  height: 40px;
+  /* border: 1px solid; */
+}
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.close-search-wrap
+  span::before {
+  content: "";
+  width: 2px;
+  height: 35px;
+  background: black;
+  position: absolute;
+  transform: rotate(45deg);
+  top: 3px;
+  left: 17px;
+}
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.close-search-wrap
+  span::after {
+  content: "";
+  width: 2px;
+  height: 35px;
+  background: black;
+  position: absolute;
+  transform: rotate(-45deg);
+  top: 3px;
+  left: 17px;
+}
+
+.search-panel-wrap
+  .search-panel-content
+  .search-input-wrap
+  label.search-icon-wrap
+  svg {
+  color: white;
 }
 </style>
