0bb25bf0c081728b56e6ce893391f39ed517e54b
Alexei Fomin <1stan@DESKTOP-U84NEP9>
2019-11-11 10:15:55 +0200

    changes

--- NewsApp/global-logic-ui/src/App.vue
+++ NewsApp/global-logic-ui/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <loader :isShowLoader='isShowLoader'></loader>
+    <loader :isShowLoader="isShowLoader"></loader>
     <HeaderDesctop></HeaderDesctop>
-    <router-view/>
+    <router-view />
     <Footer></Footer>
   </div>
 </template>
@@ -18,11 +18,11 @@
 </style>
 
 <script>
-import HeaderDesctop from './shared/layouts/header/header';
-import Footer from './shared/layouts/footer/footer';
-import loader from './components/loader/loader';
-import {mapActions, mapGetters} from 'vuex'
-import EventBus from './eventBus';
+import HeaderDesctop from "./shared/layouts/header/header";
+import Footer from "./shared/layouts/footer/footer";
+import loader from "./components/loader/loader";
+import { mapActions, mapGetters } from "vuex";
+import EventBus from "./eventBus";
 
 export default {
   name: "app",
@@ -31,12 +31,12 @@ export default {
     Footer,
     loader
   },
-  computed: mapGetters(['getWeatherData']),
+  computed: mapGetters(["getWeatherData"]),
   data() {
     return {
       isShowLoader: true,
-      weatherData: null,
-    }
+      weatherData: null
+    };
   },
   mounted() {
     this.isShowLoader = false;
@@ -49,23 +49,37 @@ export default {
       this.isShowLoader = true;
     });
 
-    this.$getLocation().then(coordinates => {
-      this.$store.dispatch('getWeather', {lat: coordinates.lat, lng: coordinates.lng})
-    }).catch(function () {
-      this.$store.dispatch('getWeather', {lat: 51.51062085840897, lng: -0.12035208720763535})
-    })
+    this.$getLocation()
+      .then(coordinates => {
+        this.$store.dispatch("getWeather", {
+          lat: coordinates.lat,
+          lng: coordinates.lng
+        });
+      })
+      .catch(function() {
+        this.$store.dispatch("getWeather", {
+          lat: 51.51062085840897,
+          lng: -0.12035208720763535
+        });
+      });
 
-    this.$store.dispatch('getTopNews', {filter: 'pageSize=6&'});
+    this.$store.dispatch("getTopNews", { filter: "pageSize=6&" });
 
-    this.$store.dispatch('getRates', {currentRate: 'USD', exchangeName: 'EUR', rateAmount: 100});
+    this.$store.dispatch("getRates", {
+      currentRate: "USD",
+      exchangeName: "EUR",
+      rateAmount: 100
+    });
   },
   methods: {
-    ...mapActions([
-      'getWeather',
-      'getRates',
-      'getNewsForHome',
-      'getTopNews',
-    ])
-  },
+    ...mapActions(["getWeather", "getRates", "getNewsForHome", "getTopNews"])
+  }
 };
 </script>
+
+<style scoped>
+#app {
+  min-height: 100vh;
+  background-color: #052962;
+}
+</style>
