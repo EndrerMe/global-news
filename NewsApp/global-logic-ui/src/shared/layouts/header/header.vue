4aadcc690a25ddf14089e54297876144ba46baa4
Alexei Fomin <1stan@DESKTOP-U84NEP9>
2019-11-13 16:34:53 +0200

    mob menu button color

--- NewsApp/global-logic-ui/src/shared/layouts/header/header.vue
+++ NewsApp/global-logic-ui/src/shared/layouts/header/header.vue
@@ -337,6 +337,12 @@ export default {
 
 <style>
 @import "./../../../assets/css/fonts.css";
+
+/* Bootstrap menu button color */
+.top-menu-wrap .mobile-top-menu .navbar-toggler .navbar-toggler-icon{
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(248, 198, 26, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E") !important;
+}
+
 </style>
 
 <style scoped>
